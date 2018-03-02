4534a9181459030756ab36417121e70cd66a9727
Nahuel Lattessi <nahuel.lattessi@etermax.com>
2018-03-02 15:24:42 -0300

    add score, add game over

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(640, 320, Phaser.AUTO, "", {
+var game = new Phaser.Game(640, 354, Phaser.AUTO, "", {
   preload: preload,
   create: create,
   update: update
@@ -22,6 +22,12 @@ var skeletonRate = 1600;
 
 var weapons = [];
 var currentWeapon = 0;
+
+var score = 0;
+var scoreText;
+
+var showReturn = false;
+var returnText;
 
 function move(deltaY) {
   if (!canMove) {
@@ -74,9 +80,44 @@ function spawnSkeleton() {
   nextSkeleton = game.time.time + skeletonRate;
 }
 
-function weaponSkeletonCollisionHandler(currentWeapon, skeleton) {
+function currentWeaponSkeletonCollisionHandler(currentWeapon, skeleton) {
   currentWeapon.kill();
   skeleton.kill();
+
+  score += 10;
+  scoreText.text = "Score: " + score;
+}
+
+function playerSkeletonsCollisionHandler(player, skeleton) {
+  player.kill();
+  displayEnd();
+}
+
+function displayEnd() {
+  // you can't win and lose at the same time
+  if (endText && endText.exists) {
+    return;
+  }
+
+  var msg = "Game Over!";
+  var endText = game.add.text(game.width / 2, game.height / 2 - 60, msg, {
+    font: "72px serif",
+    fill: "#fff"
+  });
+  endText.anchor.setTo(0.5, 0);
+  showReturn = game.time.now + Phaser.Timer.SECOND * 2;
+}
+
+function resetGame() {
+  player.destroy();
+  skeletons.destroy();
+  weapons.forEach(element => {
+    element.destroy();
+  });
+  scoreText.destroy();
+  returnText.destroy();
+
+  game.state.restart();
 }
 
 function create() {
@@ -89,16 +130,22 @@ function create() {
 
   player = game.add.sprite(32, 32, "bowman");
   player.anchor.set(0.5);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
 
   skeletons = game.add.group();
   skeletons.enableBody = true;
   skeletons.physicsBodyType = Phaser.Physics.ARCADE;
-  skeletons.createMultiple(20, "skeleton");
-  skeletons.callAll("anchor.set", "anchor", 0.5);
-  skeletons.setAll("scale.x", -1);
+  for (var i = 0; i < 20; i++) {
+    skeletons.add(new Skeleton(game, "skeleton"), true);
+  }
 
   weapons.push(new Weapon.SingleArrow(game));
   currentWeapon = 0;
+
+  scoreText = game.add.text(16, 298, "score: 0", {
+    fontSize: "32px",
+    fill: "#fff"
+  });
 }
 
 function update() {
@@ -109,10 +156,35 @@ function update() {
   game.physics.arcade.overlap(
     weapons[currentWeapon],
     skeletons,
-    weaponSkeletonCollisionHandler,
+    currentWeaponSkeletonCollisionHandler,
     null,
     this
   );
+
+  game.physics.arcade.overlap(
+    player,
+    skeletons,
+    playerSkeletonsCollisionHandler,
+    null,
+    this
+  );
+
+  if (game.input.keyboard.isDown(Phaser.Keyboard.Z)) {
+    if (returnText && returnText.exists) {
+      resetGame();
+    }
+  }
+
+  if (showReturn && game.time.now > showReturn) {
+    returnText = game.add.text(
+      game.width / 2,
+      game.height / 2 + 20,
+      "Press Z or Tap Game to go back to Main Menu",
+      { font: "16px sans-serif", fill: "#fff" }
+    );
+    returnText.anchor.setTo(0.5, 0.5);
+    showReturn = false;
+  }
 }
 
 function render() {
